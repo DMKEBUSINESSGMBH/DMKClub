6099b1c98313859247d57ec7461b36d2fa8cb4ee
René Nitzsche <rene@system25.de>
2015-03-17 09:01:35 +0100

    Bugfixes

---
+++ src/DMKClub/Bundle/PublicRelationBundle/Controller/Api/Rest/PRContactController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace DMKClub\Bundle\PublicRelationBundle\Controller\Api\Rest;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use FOS\RestBundle\Controller\Annotations\NamePrefix;
+use FOS\RestBundle\Controller\Annotations\RouteResource;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\Rest\Util\Codes;
+
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+use Oro\Bundle\SecurityBundle\Annotation\Acl;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+use Oro\Bundle\AddressBundle\Entity\AbstractTypedAddress;
+use Oro\Bundle\SoapBundle\Controller\Api\Rest\RestController;
+
+/**
+ * @NamePrefix("oro_api_")
+ */
+class PRContactController extends RestController implements ClassResourceInterface
+{
+    /**
+     * REST DELETE
+     *
+     * @param int $id
+     *
+     * @ApiDoc(
+     *      description="Delete P/R Contact",
+     *      resource=true
+     * )
+     * @Acl(
+     *      id="dmkclub_prcontact_delete",
+     *      type="entity",
+     *      permission="DELETE",
+     *      class="DMKClubPublicRelationBundle:PRContact"
+     * )
+     * @return Response
+     */
+    public function deleteAction($id)
+    {
+        return $this->handleDeleteRequest($id);
+    }
+
+    /**
+     * Get entity Manager
+     *
+     * @return ApiEntityManager
+     */
+    public function getManager()
+    {
+        return $this->get('dmkclub.prcontact.manager.api');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getForm()
+    {
+        throw new \BadMethodCallException('FormInterface is not available.');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormHandler()
+    {
+        throw new \BadMethodCallException('FormHandler is not available.');
+    }
+}
--- src/DMKClub/Bundle/PublicRelationBundle/Controller/PRContactController.php
+++ src/DMKClub/Bundle/PublicRelationBundle/Controller/PRContactController.php
@@ -4,10 +4,10 @@ namespace DMKClub\Bundle\PublicRelationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
-        return $this->render('DMKClubPublicRelationBundle:Default:index.html.twig', array('name' => $name));
-    }
+class PRContactController extends Controller {
+	public function indexAction($name) {
+		return $this->render ( 'DMKClubPublicRelationBundle:Default:index.html.twig', array (
+				'name' => $name 
+		) );
+	}
 }
