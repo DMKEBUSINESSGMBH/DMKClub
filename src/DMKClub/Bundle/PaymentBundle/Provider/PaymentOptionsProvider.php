b6bf38dd2a4dfc161ebbcf304560cacb455fa09c
René Nitzsche <rene@system25.de>
2015-10-10 12:05:42 +0200

    Payment option none added

--- src/DMKClub/Bundle/PaymentBundle/Provider/PaymentOptionsProvider.php
+++ src/DMKClub/Bundle/PaymentBundle/Provider/PaymentOptionsProvider.php
@@ -16,7 +16,8 @@ class PaymentOptionsProvider
      * @var array
      */
     protected $choices = array(
-        PaymentOption::SEPA_DIRECT_DEBIT   => 'dmkclub.payment_option.sepa_direct_debit',
+        PaymentOption::NONE  => 'dmkclub.payment_option.none',
+    		PaymentOption::SEPA_DIRECT_DEBIT   => 'dmkclub.payment_option.sepa_direct_debit',
         PaymentOption::BANKTRANSFER   => 'dmkclub.payment_option.banktransfer',
         PaymentOption::CREDITCARD   => 'dmkclub.payment_option.creditcard',
     );
