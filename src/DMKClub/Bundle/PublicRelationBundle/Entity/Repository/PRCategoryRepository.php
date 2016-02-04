06a914cd45e5a2cc493719962fc040f1f214f586
René Nitzsche <nitzsche@dmk-ebusiness.de>
2016-02-04 12:57:45 +0100

    Repos added

---
+++ src/DMKClub/Bundle/PublicRelationBundle/Entity/Repository/PRCategoryRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DMKClub\Bundle\PublicRelationBundle\Entity\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class PRCategoryRepository extends EntityRepository {
+
+}
---
+++ src/DMKClub/Bundle/PublicRelationBundle/Entity/Repository/PRContactRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DMKClub\Bundle\PublicRelationBundle\Entity\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class PRContactRepository extends EntityRepository {
+
+}
