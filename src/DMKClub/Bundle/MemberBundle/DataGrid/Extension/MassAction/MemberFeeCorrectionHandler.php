bf3e35865f477f88834f0af35d8b41763c10e167
René Nitzsche <nitzsche@dmk-ebusiness.de>
2016-05-19 19:03:18 +0200

    New mass action for fee correction

---
+++ src/DMKClub/Bundle/MemberBundle/DataGrid/Extension/MassAction/MemberFeeCorrectionHandler.php
@@ -0,0 +1,18 @@
+<?php
+namespace DMKClub\Bundle\MemberBundle\DataGrid\Extension\MassAction;
+
+use Oro\Bundle\DataGridBundle\Extension\MassAction\MassActionHandlerInterface;
+use Oro\Bundle\DataGridBundle\Extension\MassAction\MassActionHandlerArgs;
+
+class MemberFeeCorrectionHandler implements MassActionHandlerInterface {
+
+	/**
+	 * https://github.com/orocommerce/orocommerce/tree/62ce38756ca325cd9ccff708f2f9767accdd71af/src/OroB2B/Bundle/ShoppingListBundle/Datagrid/Extension/MassAction
+	 * {@inheritDoc}
+	 */
+	public function handle(MassActionHandlerArgs $args) {
+
+	}
+
+
+}
