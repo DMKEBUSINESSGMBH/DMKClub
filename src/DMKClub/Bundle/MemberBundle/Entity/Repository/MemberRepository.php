36e9c2a2f0f15edaea2b17cf2be52b5cb64b784c
René Nitzsche <nitzsche@dmk-ebusiness.de>
2016-10-18 14:23:02 +0200

    widgets for fixed for postgresql

--- src/DMKClub/Bundle/MemberBundle/Entity/Repository/MemberRepository.php
+++ src/DMKClub/Bundle/MemberBundle/Entity/Repository/MemberRepository.php
@@ -2,10 +2,7 @@
 
 namespace DMKClub\Bundle\MemberBundle\Entity\Repository;
 
-use Oro\Bundle\SecurityBundle\ORM\Walker\AclHelper;
-
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\Query\ResultSetMapping;
 
 class MemberRepository extends EntityRepository {
 	const MEMBER_TYPE_ALL = 'all';
@@ -22,7 +19,7 @@ class MemberRepository extends EntityRepository {
 		// WHERE end_date IS NULL
 		// GROUP BY is_active
 		// select statuses
-    $qb = $this->createQueryBuilder('m');
+		$qb = $this->createQueryBuilder('m');
 		$qb->select('m.isActive, count(m.id) cnt')
 			->where('m.endDate IS NULL')
 			->groupBy('m.isActive');
@@ -81,14 +78,25 @@ class MemberRepository extends EntityRepository {
 // 		ORDER BY age desc
 
 		// build rsm here
-		$sql = 'SELECT count(m.id) cnt, FLOOR(TIMESTAMPDIFF(YEAR, c.birthday, CURDATE())/10) AS age
+		if($this->isMysql()) {
+			$sql = 'SELECT count(m.id) cnt, FLOOR(TIMESTAMPDIFF(YEAR, c.birthday, CURDATE())/10) AS age
 		FROM `dmkclub_member` m
 		JOIN orocrm_contact c ON c.id = m.contact_id
 		WHERE  TIMESTAMPDIFF(YEAR, c.birthday, CURDATE())  > 0 AND TIMESTAMPDIFF(YEAR, c.birthday, CURDATE())  <= 110 ';
-		$sql .= $this->getMemberTypeClause($memberType, 'm.');
-		$sql .= ' GROUP BY FLOOR(TIMESTAMPDIFF(YEAR, c.birthday, CURDATE())/10)
-		ORDER BY age desc
-				';
+			$sql .= $this->getMemberTypeClause($memberType, 'm.');
+			$sql .= ' GROUP BY FLOOR(TIMESTAMPDIFF(YEAR, c.birthday, CURDATE())/10)';
+
+		}
+		else {
+			$sql = 'SELECT count(m.id) cnt, FLOOR(EXTRACT(YEAR FROM age(c.birthday))/10) AS age
+		FROM dmkclub_member m
+		JOIN orocrm_contact c ON c.id = m.contact_id
+		WHERE c.birthday IS NOT NULL ';
+			$sql .= $this->getMemberTypeClause($memberType, 'm.');
+			$sql .= 'GROUP BY FLOOR(EXTRACT(YEAR FROM age(c.birthday))/10)
+					';
+		}
+		$sql .= ' ORDER BY age desc';
 
 		$stmt = $this->_em->getConnection()->prepare($sql);
 		$stmt->execute();
@@ -104,17 +112,25 @@ class MemberRepository extends EntityRepository {
 		return $resultData;
 	}
 
+	protected function isMysql() {
+		return $this->_em->getConnection()->getDatabasePlatform()->getName() == 'mysql';
+	}
 	public function getNewMembersByYear($memberType) {
-
+		// Die Abfrage ist abhängig von der Datenbank
+		$isMysql = $this->isMysql();
 		// build rsm here
-		$sql = 'SELECT YEAR(start_date) year, count(id) cnt
-				FROM dmkclub_member WHERE 1=1 ';
-		$sql .= $this->getMemberTypeClause($memberType, '');
-		$sql .=	' GROUP BY YEAR(start_date)
-				ORDER BY YEAR(start_date) desc
-				LIMIT 20
+		$sql = $isMysql ? 'SELECT YEAR(start_date) yr, count(id) cnt
+				'
+				:
+				'SELECT EXTRACT(YEAR FROM start_date) yr, count(id) cnt
 				';
-
+		$sql .= 'FROM dmkclub_member WHERE 1=1';
+		$sql .= $this->getMemberTypeClause($memberType, '');
+		$sql .=	$isMysql ?
+					' GROUP BY YEAR(start_date) ORDER BY YEAR(start_date) desc'
+					:
+					' GROUP BY EXTRACT(YEAR FROM start_date) ORDER BY EXTRACT(YEAR FROM start_date) desc';
+		$sql .=	' LIMIT 20';
 		$stmt = $this->_em->getConnection()->prepare($sql);
 		$stmt->execute();
 		$data = $stmt->fetchAll();
@@ -123,7 +139,7 @@ class MemberRepository extends EntityRepository {
  		$resultData = array();
 		foreach ($data as $row) {
 			$resultData[] = [
-				'label' => $row['year'],
+				'label' => $row['yr'],
 				'value' => (int)$row['cnt'],
 			];
 		}
