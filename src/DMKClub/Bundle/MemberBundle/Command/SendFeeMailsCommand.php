746f70f290a388708b0d2bd7f44311ec49935263
René Nitzsche <nitzsche@dmk-ebusiness.de>
2017-10-20 19:05:35 +0200

    Code style

--- src/DMKClub/Bundle/MemberBundle/Command/SendFeeMailsCommand.php
+++ src/DMKClub/Bundle/MemberBundle/Command/SendFeeMailsCommand.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace DMKClub\Bundle\MemberBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -14,96 +13,100 @@ use DMKClub\Bundle\MemberBundle\Mailer\Processor;
 use DMKClub\Bundle\BasicsBundle\Model\TemplateNotFoundException;
 
 /**
+ * Send membership fees to member by email.
  */
-class SendFeeMailsCommand extends ContainerAwareCommand {
+class SendFeeMailsCommand extends ContainerAwareCommand
+{
 
     const NAME = 'dmkclub:send:fee';
+
     const FLUSH_BATCH_SIZE = 100;
 
-	protected $output;
+    protected $output;
 
-	/**
-	 *
-	 * @return ConfigManager
-	 */
-	protected function configure() {
-		$this->setName(self::NAME)
-			->setDescription('Command to send a number of fees by email to member')
-			->addArgument('ids', InputArgument::REQUIRED, 'commaseperated id of fees to send')
-		;
-	}
-	/**
-	 *
-	 * @return EntityManager
-	 */
-	protected function getEntityManager()
-	{
-	    return $this->getContainer()->get('doctrine.orm.entity_manager');
-	}
-	/**
-	 *
-	 * @return Processor
-	 */
-	protected function getMailer()
-	{
-	    return $this->getContainer()->get('dmkclub_member.mailer.processor');
-	}
+    /**
+     *
+     * @return ConfigManager
+     */
+    protected function configure()
+    {
+        $this->setName(self::NAME)
+            ->setDescription('Command to send a number of fees by email to member')
+            ->addArgument('ids', InputArgument::REQUIRED, 'commaseperated id of fees to send');
+    }
 
-	protected function execute(InputInterface $input, OutputInterface $output) {
+    /**
+     *
+     * @return EntityManager
+     */
+    protected function getEntityManager()
+    {
+        return $this->getContainer()->get('doctrine.orm.entity_manager');
+    }
 
+    /**
+     *
+     * @return Processor
+     */
+    protected function getMailer()
+    {
+        return $this->getContainer()->get('dmkclub_member.mailer.processor');
+    }
 
-	    $this->initLanguage();
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->initLanguage();
 
-	    $this->output = $output;
-		$ids = $input->getArgument('ids');
-		try {
-		    $result = $this->processEmails(explode(',', $ids));
-		    $output->writeln(sprintf('<info>Job finished</info> with %d processed fees and %d errors', $result['cnt'], $result['errors']));
-		} catch (Exception $e) {
-		    $output->writeln(sprintf('<error>Job failed</error>'));
-		    $output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
-		}
-	}
-	protected function processEmails($feeIds)
-	{
-	    $iteration = 0;
-	    $errorCnt = 0;
-	    foreach ($feeIds as $feeId) {
-	        $iteration ++;
-	        $entity = $this->getEntityManager()->getRepository(MemberFee::class)->find($feeId);
+        $this->output = $output;
+        $ids = $input->getArgument('ids');
+        try {
+            $result = $this->processEmails(explode(',', $ids));
+            $output->writeln(sprintf('<info>Job finished</info> with %d processed fees and %d errors', $result['cnt'], $result['errors']));
+        } catch (Exception $e) {
+            $output->writeln(sprintf('<error>Job failed</error>'));
+            $output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
+        }
+    }
+
+    protected function processEmails($feeIds)
+    {
+        $iteration = 0;
+        $errorCnt = 0;
+        foreach ($feeIds as $feeId) {
+            $iteration ++;
+            $entity = $this->getEntityManager()
+                ->getRepository(MemberFee::class)
+                ->find($feeId);
             try {
                 // SEND EMAIL
                 $this->getMailer()->sendBillToMemberEmail($entity);
-            }
-            catch (TemplateNotFoundException $tnfe) {
+            } catch (TemplateNotFoundException $tnfe) {
                 // Ohne Template wird keine Mail verschickt werden.
                 throw $tnfe;
-            }
-            catch (\Exception $e) {
-                $this->output->writeln(sprintf('Fee mailing failed for member %s (%s), fee-id: %d, Error: %s',
-                    $entity->getMember()->getName(),
-                    $entity->getMember()->getId(),
-                    $entity->getId(),
-                    $e->getMessage()
-                ));
+            } catch (\Exception $e) {
+                $this->output->writeln(sprintf('Fee mailing failed for member %s (%s), fee-id: %d, Error: %s', $entity->getMember()
+                    ->getName(), $entity->getMember()
+                    ->getId(), $entity->getId(), $e->getMessage()));
                 // Diese Exception wird nicht weitergegeben, damit der Rest versendet wird
                 $errorCnt += 1;
             }
 
+            if (($iteration % self::FLUSH_BATCH_SIZE) === 0) {
+                $this->getEntityManager()->flush();
+                $this->getEntityManager()->clear();
+            }
+        }
+        return [
+            'cnt' => $iteration,
+            'errors' => $errorCnt
+        ];
+    }
 
-	        if (($iteration % self::FLUSH_BATCH_SIZE) === 0) {
-	            $this->getEntityManager()->flush();
-	            $this->getEntityManager()->clear();
-	        }
-	    }
-	    return ['cnt' => $iteration, 'errors' => $errorCnt];
-	}
-
-	protected function initLanguage()
-	{
-	    /* @var \Oro\Bundle\ConfigBundle\Config\ConfigManager */
-	    $configManager = $this->getContainer()->get('oro_config.global');
-	    $t = $this->getContainer()->get('translator');
-	    $t->setLocale($configManager->get('oro_locale.language'));
-	}
+    protected function initLanguage()
+    {
+        /* @var \Oro\Bundle\ConfigBundle\Config\ConfigManager */
+        $configManager = $this->getContainer()->get('oro_config.global');
+        $t = $this->getContainer()->get('translator');
+        $t->setLocale($configManager->get('oro_locale.language'));
+    }
 }
