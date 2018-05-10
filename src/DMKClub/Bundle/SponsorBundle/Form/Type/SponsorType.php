2d28188e0d441d33eb9dd20ec87230f5e864ffc3
René Nitzsche <nitzsche@dmk-ebusiness.de>
2018-05-10 12:42:01 +0200

    Some fixes for sponsors

--- src/DMKClub/Bundle/SponsorBundle/Form/Type/SponsorType.php
+++ src/DMKClub/Bundle/SponsorBundle/Form/Type/SponsorType.php
@@ -7,107 +7,90 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class SponsorType extends AbstractType
 {
+    const LABEL_PREFIX = 'dmkclub.sponsor.';
+
     /**
+     *
      * @param FormBuilderInterface $builder
-     * @param array                $options
+     * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $this->buildPlainFields($builder, $options);
         $this->buildRelationFields($builder, $options);
     }
+
     /**
+     *
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    protected function buildPlainFields(FormBuilderInterface $builder, array $options) {
-    	$builder
-
-        ->add('name', 'text', array('required' => true, 'label' => 'dmkclub.member.name.label'))
-
-    	->add('isActive')
-    	->add('owner')
-    	->add('organization')
-    	;
-        $builder->add(
-            'contact',
-            'orocrm_contact_select',
-            [
-                'label'    => 'orocrm.sales.b2bcustomer.contact.label',
+    protected function buildPlainFields(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', 'text', array(
                 'required' => true,
-            ]
-        );
-        $builder->add(
-            'account',
-            'orocrm_account_select',
-            [
-                'label'    => 'orocrm.sales.b2bcustomer.account.label',
-                'required' => true,
-            ]
-        );
+                'label' => self::LABEL_PREFIX . 'name.label'
+            ))
+            ->add('isActive')
+            ->add('owner')
+            ->add('organization');
+        $builder->add('contact', 'oro_contact_select', [
+            'label' => self::LABEL_PREFIX . 'contact.label',
+            'required' => true
+        ]);
+        $builder->add('account', 'oro_account_select', [
+            'label' => self::LABEL_PREFIX . 'account.label',
+            'required' => false
+        ]);
     }
+
     /**
+     *
      * @param FormBuilderInterface $builder
      * @param array $options
      * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
-    public function buildRelationFields(FormBuilderInterface $builder, array $options){
+    public function buildRelationFields(FormBuilderInterface $builder, array $options)
+    {
         // tags removed in 1.9
-        //$builder->add('tags', 'oro_tag_select', array('label' => 'oro.tag.entity_plural_label'));
-
-        $builder->add(
-        		'dataChannel',
-        		'orocrm_channel_select_type',
-        		[
-        		'required' => true,
-        		'label'    => 'orocrm.sales.b2bcustomer.data_channel.label',
-        		'entities' => [
-	        		'DMKClub\\Bundle\\SponsorBundle\\Entity\\Sponsor'
-	        		],
-        		]
-        );
+        // $builder->add('tags', 'oro_tag_select', array('label' => 'oro.tag.entity_plural_label'));
+        $builder->add('dataChannel', 'oro_channel_select_type', [
+            'required' => true,
+            'label' => self::LABEL_PREFIX . 'data_channel.label',
+            'entities' => [
+                'DMKClub\\Bundle\\SponsorBundle\\Entity\\Sponsor'
+            ]
+        ]);
 
         // sponsor categories
-        $builder->add(
-        		'category',
-        		'dmkclub_sponsorcategory_select',
-        		array(
-                   'label'    => 'dmkclub.sponsor.category.entity_label',
-                   'required' => false,
-        		)
-        );
-        $builder->add(
-        		'billingAddress',
-        		'oro_address',
-        		[
-        		'cascade_validation' => true,
-        		'required'           => false
-        		]
-        );
-        $builder->add(
-        		'postalAddress',
-        		'oro_address',
-        		[
-        				'cascade_validation' => true,
-        				'required'           => false
-        		]
-        );
-
+        $builder->add('category', 'dmkclub_sponsorcategory_select', array(
+            'label' => self::LABEL_PREFIX . 'category.entity_label',
+            'required' => false
+        ));
+        $builder->add('billingAddress', 'oro_address', [
+            'cascade_validation' => true,
+            'required' => false
+        ]);
+        $builder->add('postalAddress', 'oro_address', [
+            'cascade_validation' => true,
+            'required' => false
+        ]);
     }
+
     /**
+     *
      * @param OptionsResolverInterface $resolver
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults(
-            array(
-                'data_class' => 'DMKClub\Bundle\SponsorBundle\Entity\Sponsor',
-                'cascade_validation' => true,
-            )
-        );
+        $resolver->setDefaults(array(
+            'data_class' => 'DMKClub\Bundle\SponsorBundle\Entity\Sponsor',
+            'cascade_validation' => true
+        ));
     }
 
     /**
+     *
      * @return string
      */
     public function getName()
